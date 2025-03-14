7090e6cbdbbd816690c90d25e7daa8935dc2598d
Adham <adhambiko22@gmail.com>
2025-03-14 11:39:33 +0200

    Setup Project's MVVM Structure

--- app/src/main/java/com/example/tempora/MainActivity.kt
+++ app/src/main/java/com/example/tempora/MainActivity.kt
@@ -16,32 +16,9 @@ import com.example.tempora.ui.theme.TEMPORATheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TEMPORATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TEMPORATheme {
-        Greeting("Android")
-    }
-}
